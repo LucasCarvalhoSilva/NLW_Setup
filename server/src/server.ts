a88582d853922cfb96c2d25a22695981b8f1dd24
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2023-01-19 11:03:46 -0300

    Aula 2 - Avançando o back-end e o front-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,13 @@
 import Fastify from "fastify"
 import cors from "@fastify/cors"
-
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "./lib/prisma"
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/hello', async () => {
-  const habits = await prisma.Habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Estudar'
-      }
-    }
-  })
-
-  return habits
-})
 
 app.listen({
   port: 3333,
